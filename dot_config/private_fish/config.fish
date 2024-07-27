003dfb9b1f7e7470b063b681615304bb0606f29f
Daniel Nowak <dnowak@thehive.ai>
2024-07-26 17:28:03 -0700

    Adding new dotfiles

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    set -U fish_greeting
+    set -x LANG en_US.UTF-8
+    fzf --fish | source
+    zoxide init --cmd cd fish | source
+    starship init fish | source
+    eval (thefuck --alias | tr '\n' ';')
+    abbr --add cat bat
+    abbr --add ls eza --color=always --icons=always
+    abbr --add ll eza --color=always --icons=always --long
+end
