307f87ddfb8f7102f6c6d033f8c7f42f9abc181c
Daniel Nowak <danielnowak@veryfi.com>
2025-04-09 10:59:12 -0700

    Clean up and sync: remove brew script, update fish + wezterm configs

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -4,9 +4,6 @@ if status is-interactive
     fzf --fish | source
     zoxide init --cmd cd fish | source
     starship init fish | source
-    eval (thefuck --alias | tr '\n' ';')
-    abbr --add cat bat
     abbr --add ls eza --color=always --icons=always
     abbr --add ll eza --color=always --icons=always --long
-    pyenv init - | source
 end
--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -7,8 +7,8 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Apathy (base16)'
-config.font = wezterm.font 'ComicCodeLigatures Nerd Font'
+config.color_scheme = 'SynthwaveAlpha'
+config.font = wezterm.font 'Codelia NF'
 config.font_size = 16
 
 -- and finally, return the configuration to wezterm
